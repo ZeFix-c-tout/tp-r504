2336c38f0912e26630bf84f41a46def4f6b7790b
Your Name <you@exemple.com>
2025-09-04 16:57:38 +0200

    Q4.6

--- TP1/functions.py
+++ TP1/functions.py
@@ -2,7 +2,7 @@ def puissance(a,b):
 	if not type(a) is int:
 		raise TypeError("Only integers are allowed")
 
-	elif a !< 0 or b !< 0:
+	elif a == -1 or b == -1:
 		return ('La valeur doit être suppérieur à 0')
 	else:
 		return(a**b)
