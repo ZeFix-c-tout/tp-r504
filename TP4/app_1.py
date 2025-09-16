87138954899afe75ec19a4fbc6a63b3d9e60c654
your name <you@example.com>
2025-09-16 07:45:23 -0400

    Q

--- TP4/app_1.py
+++ TP4/app_1.py
@@ -18,14 +18,16 @@ cursor = conn.cursor()
 
 @app.route('/')
 def index():
+	conn = mysql.connector.connect(**db_config)
+	cursor = conn.cursor() 
     # Sample query
     query = "SELECT * FROM myTable"
     cursor.execute(query)
     data = cursor.fetchall()
     
     # Close the cursor and connection
-    cursor.close()
-    conn.close()
+    #cursor.close()
+    #conn.close()
     
     return render_template('index.html', data=data)
 
--- TP4/purge.sh
+++ TP4/purge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker stop $(docker ps -aq)
 docker rm  $(docker ps -aq)
-#docker volume prune -a
-#docker system prune -a
+docker volume prune -a
+docker system prune -a
